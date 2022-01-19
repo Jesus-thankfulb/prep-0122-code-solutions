dcccb9aa109a7a8a17139b244c3b5ba59b30c1a5
“Jesus <martinezj1992@yahoo.com>
2022-01-19 11:02:02 -0600

    Completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,11 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('Language Arrays', languages);
+
+const firstElement = languages[0];
+console.log('the first element of the array is', firstElement);
+const thridElement = languages[2];
+console.log('the third element of the array is', thridElement);
+const length = languages.length;
+console.log('the length of the array is', length);
+const lastElement = languages[languages.length - 1];
+console.log('the last element of the array is', lastElement);
