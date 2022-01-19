8a5bab29c81f8ee7d86f31071c8bb19903ddc6bb
“Jesus <martinezj1992@yahoo.com>
2022-01-18 20:56:47 -0600

    Completed prep-javascript-objects

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,14 @@
+const person = {
+  firstName: 'Steve',
+  lastName: 'Martin',
+  hobby: 'Banjo Playing'
+};
+
+console.log(person);
+const fullName = 'Steve ' + 'Martin';
+console.log("The person's name is:", fullName);
+person.job = 'Jerk';
+console.log("The person's current job is:", person.job);
+person.previous_Job = 'amgio';
+console.log("The person's previous job is:", person.previous_Job);
+console.log('The complete person:', person);
